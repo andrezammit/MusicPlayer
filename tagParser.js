281c1170f3b97937a5780bc33a5188652ff740cf
mouzer <mawzer@gmail.com>
2014-07-21 01:58:32 +0200

    Minor improvements and cleanup of tagParser.js.

--- tagParser.js
+++ tagParser.js
@@ -31,7 +31,7 @@ function getTag(fullPath, callback)
 		if (buffer.length < 3)
 			return false;
 
-		var tagID = buffer.toString('utf-8', 0, 3);
+		var tagID = buffer.toString('utf8', 0, 3);
 
 		console.log(tagID);
 
@@ -59,38 +59,12 @@ function getTag(fullPath, callback)
 		}
 		else
 		{
-			// var hexLen = [];
-			// hexLen[0] = buffer[6] & 0x7F;
-			// hexLen[1] = buffer[7] & 0x7F;
-			// hexLen[2] = buffer[8] & 0x7F;
-			// hexLen[3] = buffer[9];
-
-			// var tmpByte = hexLen[3];
-			// tmpByte = tmpByte | 0x80;
-
-			// for (cnt = 3; cnt >= 0; cnt--)
-			// {
-			// 	var shift = 3 - cnt;
-			// 	tagLen += hexLen[cnt] << 8 * shift;
-			// }
-
-			// tagLen = tagLen >> 1;
-			// tagLen = tagLen & tmpByte;
-
-			// tagLen = buffer.readUInt32BE(6);
-			
-			// var tmpFirstByte = tagLen & 0xFF;
-			// tagLen &= (~0xFF) & tagLen;
-
-			// tagLen = tagLen >> 1;
-			// tagLen |= tmpFirstByte;
-
 			tagLen = buffer.readUInt32BE(6);
 
 			var tmpValue = tagLen & 0x7F;
 			for (lineCount = 1; lineCount < 4; lineCount++)
 			{
-				var tmpByte = (0x7F << (8*lineCount));
+				var tmpByte = (0x7F << (8 * lineCount));
 				tmpValue = ((tagLen & tmpByte) >> lineCount) + tmpValue;
 			}
 
@@ -143,15 +117,13 @@ function getTag(fullPath, callback)
 		fs.read(fd, dataBuffer, 0, tagLen, 10, 
 			function(error, bytesRead) 
 			{
+				fs.closeSync(fd);
+				
 				if (error)
 				{
 					returnError(error);
 					return;
 				}
-
-				fs.closeSync(fd);
-
-				//console.log(buffer.toString('utf-8', 0, tagLen));
 
 				callback(dataBuffer);
 			});
@@ -194,18 +166,11 @@ function getTag(fullPath, callback)
 
 	function getTagFrame(tagData, offset, callback)
 	{
-		//console.log(fullPath);
-
-		// try
-		// {
 		var frameID = tagData.toString('utf8', offset, offset + 4);
 		offset += 4;
 
 		if (frameID.charCodeAt(3) == 0)
 			frameID = frameID.substring(0, 3);
-		
-		if (frameID.charCodeAt(0) != 0)
-			console.log(frameID);
 
 		var frameSize = tagData.readUInt32BE(offset);
 		offset += 4;
@@ -213,28 +178,45 @@ function getTag(fullPath, callback)
 		var frameFlags = tagData.readUInt16BE(offset);
 		offset += 2;
 
-		var dataSize = frameSize;
+		var frameData;
 
-		var frameEncoding = 'utf8';
-		if (isEncodedFrame(frameID))
+		if (!isIgnoreFrame(frameID))
 		{
-			var frameEncodingByte = tagData.readUInt8(offset);
-			
-			offset += 1;
-			dataSize -= 1;
+			console.log(frameID);
 
-			//frameEncoding = getFrameEncodingType(frameEncodingByte);
+			var dataSize = frameSize;
+
+			var frameEncoding = 'utf8';
+			if (isEncodedFrame(frameID))
+			{
+				var frameEncodingByte = tagData.readUInt8(offset);
+				
+				offset += 1;
+				dataSize -= 1;
+
+				//frameEncoding = getFrameEncodingType(frameEncodingByte);
+			}
+
+			frameData = tagData.toString(frameEncoding, offset, offset + dataSize);
 		}
 
-		var frameData = tagData.toString(frameEncoding, offset, offset + dataSize);
-
 		callback(frameID, frameSize, frameData);
-	//	}
-	// catch (ex)
-	// {
-	// 	console.log(ex);
-	// }
-	
+	}
+
+	function isIgnoreFrame(frameID)
+	{
+		switch (frameID)
+		{
+		case 'TAL':
+		case 'TALB':
+		case 'TT2':
+		case 'TIT2':
+		case 'TP1':
+		case 'TPE1':
+			return false;
+		}
+
+		return true;
 	}
 
 	function processTagData(tag, frameID, frameData, callback)
@@ -273,7 +255,7 @@ function getTag(fullPath, callback)
 				return;
 			}
 
-			getTagFrame(tagData, offset, readTagFrameDone)
+            setTimeout(getTagFrame, 0, tagData, offset, readTagFrameDone);
 		}
 
 		function readTagFrameDone(frameID, frameSize, frameData)
