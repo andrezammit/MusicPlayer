2722370438bee4e3a5f9e97ed6b0090bb79fa42f
mouzer <mawzer@gmail.com>
2014-08-11 23:42:37 +0200

    Added gradient to playing track background.

--- Client/js/main.js
+++ Client/js/main.js
@@ -357,7 +357,7 @@ MusicPlayer.engine = (function()
 			}
 			else
 			{
-				trackElement.css('background', 'rgba(255, 255, 255, 0.2)');
+				trackElement.css('background', 'linear-gradient(to right, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.2)');
 
 				playImage.attr('src', 'images/pause.png');
 				playImage.attr('alt', 'Pause');
