1efd2e145506de60ed16745fc19b6bd4e437fcfa
mouzer <mawzer@gmail.com>
2014-11-03 00:33:15 +0100

    Cleaned up main.js.

--- Server/main.js
+++ Server/main.js
@@ -3,48 +3,13 @@ var database = require('./database');
 var webServer = require('./webserver');
 var webSocketServer = require('./webSocketServer');
 var mp3Server = require('./mp3Server');
+var tagWriter = require('./TagWriter');
 
-function saveTagsDone(tagCount)
+var fs = require('fs');
+var open = require('open');
+
+(function initialize()
 {
-    console.log('Done saving ' + tagCount + ' ID3 tags to database.');
-}
-
-function extractTagsDone(tagList)
-{
-    console.log("Done extracting ID3 tags.");
-    console.log("Saving tags to database...");
-
-    database.saveTags(tagList, saveTagsDone);
-}
-
-function scanDone(error, fileList)
-{
-	if (error)
-	{
-        console.log('Error: ' + error);
-        return;
-    }
-
-	if (!fileList)
-	{
-        console.log('Error: No files found.');
-        return;
-    }
-    
-    console.log('Finished file listing.');
-    console.log('Found ' + fileList.length + ' files.');
-
-    console.log('Extracting ID3 tags...');
-    fileSystem.extractTags(fileList, extractTagsDone)
-}
-
-//fileSystem.scan('H:\\Music\\Music', scanDone);
-//fileSystem.scan('H:\\Music\\Music\\Foo Fighters', scanDone);
-
-// new tagParser(true).getTag('H:\\Music\\Music\\Foo Fighters\\In Your Honor [Disc 1]\\1-01 In Your Honor.mp3', getTagDone);
-
-// function getTagDone(error, tag)
-// {
-//     console.log(tag);
-// }
-
+    fs.mkdir('artwork');
+    open('http://localhost:3000/');
+})();
